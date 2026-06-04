529a07fa3f648b8a3a0949ff87d8668c2320b2a0
louismorgner <louis@louismorgner.com>
2026-06-04 17:08:43 +0200

    Add OpenCompany icon assets

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -71,5 +71,7 @@ export default async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|docs(?:/.*)?).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|favicon.svg|icon-traced.svg|brand(?:/.*)?|docs(?:/.*)?).*)",
+  ],
 };
